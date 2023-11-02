93a2d920dd264729aa6e59f330a4e66aad65db93
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-11-02 15:33:44 -0600

    Se hace la conexion con la api y el front

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  usuarios : any = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:3000/usuarios').subscribe((data) => {
+      this.usuarios = data;
+    });
   }
 
+  ngOnInit() {}
 }
