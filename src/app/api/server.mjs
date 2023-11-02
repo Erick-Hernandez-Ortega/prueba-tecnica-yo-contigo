7c181cd85140370b6b61c44eafe656821e7ce99e
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-11-02 15:33:12 -0600

    Se hace la conexion con la api y el front

--- src/app/api/server.mjs
+++ src/app/api/server.mjs
@@ -1,17 +1,20 @@
 import express from 'express';
 import connection from './db.mjs';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
 app.get('/usuarios', (req, res) => {
     connection.query('SELECT * FROM usuarios', (err, results) => {
-      if (err) {
-        res.status(500).json({ error: 'Error al consultar la base de datos' });
-        return;
-      }
-      res.json(results);
+        if (err) {
+            res.status(500).json({ error: 'Error al consultar la base de datos' });
+            return;
+        }
+        res.status(200).json(results);
     });
-  });
+});
 
 app.listen(3000, () => {
     console.log('API en ejecución en el puerto 3000');
