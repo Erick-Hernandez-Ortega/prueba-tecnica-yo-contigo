afed0f7314ddf95127632c545067c85e29a5417a
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-11-02 19:11:11 -0600

    Se hace la funcionalidad de borrar

--- src/app/api/server.mjs
+++ src/app/api/server.mjs
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get('/usuarios', (req, res) => {
     connection.query('SELECT * FROM usuarios', (err, results) => {
         if (err) {
-            res.status(500).json({ error: 'Error al consultar la base de datos' });
+            res.status(500).json({ message: 'Error al consultar la base de datos', title: "Operacion Incorrectra" });
             return;
         }
         res.status(200).json(results);
@@ -32,6 +32,25 @@ app.post('/usuarios', (req, res) => {
             res.status(200).json({ message: "Se ingresado correctamente en la BD", title: "Operacion Correcta" });
         })
 })
+
+app.delete("/usuarios/:id", (req, res) => {
+    const id = req.params.id;
+
+    connection.query(`DELETE FROM prueba.usuarios WHERE id = ${id}`, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Error al consultar la base de datos', title: "Operacion Incorrectra" });
+            return;
+        }
+        res.status(200).json({ message: "Se borrado correctamente en la BD", title: "Operacion Correcta" });
+    })
+})
+
+
+app.use((req, res) => {
+    res.status(404).send("<h1>Error 404 endpoint na encontrado</h1>")
+})
+
 
 app.listen(3000, () => {
     console.log('API en ejecución en el puerto 3000');
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -7,7 +7,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  usuarios : any = [];
+  usuarios: any = [];
+  response: any = {};
+  @ViewChild('abrirModal', { static: true }) miBoton!: ElementRef;
 
   constructor(private http: HttpClient) {
     this.http.get('http://localhost:3000/usuarios').subscribe((data) => {
@@ -15,5 +17,22 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  borrar(id: number): void {
+
+    this.http
+      .delete(`http://localhost:3000/usuarios/${id}`)
+      .subscribe(
+        (response) => {
+          // Maneja la respuesta del servidor
+          this.response = response;
+        },
+        (error) => {
+          this.response = error;
+        }
+      );
+
+    this.miBoton.nativeElement.click();
+  }
+
+  ngOnInit() { }
 }
