dd272d3691ded7aba36b210b4b402728e81b13fc
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-11-02 18:44:18 -0600

    Añado modal y otra verificacion al ingresar usuario

--- src/app/components/form-users/form-users.component.ts
+++ src/app/components/form-users/form-users.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-form-users',
@@ -14,37 +13,44 @@ export class FormUsersComponent implements OnInit {
   fecha: string = '';
   sexo: string = '';
   isMenor: boolean = true;
+  response: any = {};
+  @ViewChild('abrirModal', { static: true }) miBoton!: ElementRef;
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() { }
+  ngOnInit() {}
 
   validar(): boolean {
     const regexNum = /^\d+$/;
+    const regexEmail = /^\S+@\S+\.\S+$/;
 
     // Email ya se valido con required en la plantilla
-    if (this.telefono.length === 10 && regexNum.test(this.telefono) && !this.esMenor()) {
-      return true
+    if (
+      this.telefono.length === 10 &&
+      regexNum.test(this.telefono) &&
+      !this.esMenor() &&
+      regexEmail.test(this.email)
+    ) {
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 
   esMenor(): boolean {
-    if (this.fecha == "") {
-      return true
+    if (this.fecha == '') {
+      return true;
     } else {
       const fechaNacimiento = new Date(this.fecha);
       const fechaActual = new Date();
-      const diferenciaTiempo = fechaActual.getTime() - fechaNacimiento.getTime();
+      const diferenciaTiempo =
+        fechaActual.getTime() - fechaNacimiento.getTime();
       const edad = diferenciaTiempo / (1000 * 60 * 60 * 24 * 365.25);
       const edadMinima = 18;
 
       this.isMenor = edad < edadMinima;
       return edad < edadMinima;
     }
-
   }
 
   registrar(): void {
@@ -53,25 +59,27 @@ export class FormUsersComponent implements OnInit {
       email: this.email,
       telefono: this.telefono,
       sexo: this.sexo,
-      fecha: this.fecha
-    }
+      fecha: this.fecha,
+    };
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+        'Content-Type': 'application/json',
+      }),
     };
 
-    this.http.post('http://localhost:3000/usuarios', data, httpOptions).subscribe(
-      (response) => {
-        // Maneja la respuesta del servidor
-        console.log('Respuesta:', response);
-      },
-      (error) => {
-        // Maneja errores, si los hay
-        console.error('Error:', error);
-      }
-    );
+    this.http
+      .post('http://localhost:3000/usuarios', data, httpOptions)
+      .subscribe(
+        (response) => {
+          // Maneja la respuesta del servidor
+          this.response = response;
+        },
+        (error) => {
+          this.response = error;
+        }
+      );
 
+    this.miBoton.nativeElement.click();
   }
 }
